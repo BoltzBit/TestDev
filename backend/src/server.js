5c8cc3baa897d2c091681853cde32b3094209d11
BoltzBit <mateus_desktop@outlook.com>
2021-02-02 11:51:04 -0300

    Inserção de campos do formulário

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,11 +4,9 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(express.json());
-
-app.use(express.urlencoded({extended: false}));
-
 app.use(cors());
+
+app.use(express.json());
 
 app.use(routes);
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,103 @@
-import React, { useState } from 'react';
-import { render } from 'react-dom';
-import { DatePicker, message } from 'antd';
+import React, { useState, useEffect } from 'react';
 import 'antd/dist/antd.css';
+import {
+  Form,
+  Input,
+  Button,
+  Select
+} from 'antd';
+
+import api from './services/api';
+
+const Cadastro = () => {
+  const [ form ] = Form.useForm();
+  const [componentSize, setComponentSize] = useState('default');
+  const [ tipoPessoa, setTipoPessoa ] = useState([]);
+
+  useEffect(() => {
+    api.get('pessoa')
+    .then(response => {
+      console.log(response.data)
+      setTipoPessoa(response.data);
+    });
+  },[]);
+
+  const onFormLayoutChange = ({ size }) => {
+    setComponentSize(size);
+  };
+
+  return (
+    <>
+      <Form
+        form={form}
+        labelCol={{
+          span: 4,
+        }}
+        wrapperCol={{
+          span: 14,
+        }}
+        layout="vertical"
+        initialValues={{
+          size: componentSize,
+        }}
+        onValuesChange={onFormLayoutChange}
+        size={componentSize}
+      >
+
+        <Form.Item label="Nome" name="nome">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Tipo Pessoa" name="tipopessoa">
+          <Select>
+            {tipoPessoa.map(({ id, descricao }) => 
+              <Select.Option
+                key={id}
+                value={id}
+              >
+                {descricao}
+              </Select.Option>
+            )}
+          </Select>
+        </Form.Item>
+        <Form.Item label="CPF/CNPJ" name="cpf_cnpj">
+          <Input />
+        </Form.Item>
+        <Form.Item label="CEP" name="cep">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Endereço" name="endereco">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Número" name="numero">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Bairro" name="bairro">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Cidade" name="cidade">
+          <Input />
+        </Form.Item>
+        <Form.Item label="UF" name="uf">
+          <Input />
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+          >
+            Cadastrar
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
+
+
 
 const App = () => {
-  const [date, setDate] = useState(null);
-  const handleChange = value => {
-    message.info(`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`);
-    setDate(value);
-  };
-  return (
-    <div style={{ width: 400, margin: '100px auto' }}>
-      <DatePicker onChange={handleChange} />
-      <div style={{ marginTop: 16 }}>
-        Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
-      </div>
-    </div>
-  );
+  return  <Cadastro/>;
 };
 
 export default App;
